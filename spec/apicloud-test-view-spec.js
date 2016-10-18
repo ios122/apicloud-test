e20ce03bb80cec6bb4fe5b06f7ca481b5d96d1f8
wei <weiwei@vip.qq.com>
2016-10-18 14:28:09 +0800

    1111

---
+++ spec/apicloud-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ApicloudTestView from '../lib/apicloud-test-view';
+
+describe('ApicloudTestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
